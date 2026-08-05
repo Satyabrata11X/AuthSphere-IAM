47d06bf37e318b1362e43d0ff2cafd86c93d86f9
satyabrata11x <satyabrata11x@gmail.com>
2026-08-05 20:17:46 +0530

    feat(auth): implement JWT service and authentication filter

--- src/main/java/com/authsphere/authsphere_backend/core/config/SecurityConfig.java
+++ src/main/java/com/authsphere/authsphere_backend/core/config/SecurityConfig.java
@@ -1,19 +1,25 @@
 package com.authsphere.authsphere_backend.core.config;
 
-
+import com.authsphere.authsphere_backend.identity.auth.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -22,6 +28,10 @@ public class SecurityConfig {
 
         return http
                 .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
 
                 .authorizeHttpRequests(auth -> auth
 
@@ -35,6 +45,9 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
 
                 )
+
+                .addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class)
 
                 .httpBasic(Customizer.withDefaults())
 
--- src/main/java/com/authsphere/authsphere_backend/identity/auth/security/JwtAuthenticationFilter.java
+++ src/main/java/com/authsphere/authsphere_backend/identity/auth/security/JwtAuthenticationFilter.java
@@ -1,4 +1,79 @@
 package com.authsphere.authsphere_backend.identity.auth.security;
 
-public class JwtAuthenticationFilter {
+import com.authsphere.authsphere_backend.identity.auth.service.JwtService;
+import com.authsphere.authsphere_backend.identity.user.User;
+import com.authsphere.authsphere_backend.identity.user.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+
+        String jwt = authHeader.substring(7);
+
+        String username = jwtService.extractUsername(jwt);
+
+        if (username != null
+                && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            Optional<User> optionalUser = userRepository.findByEmail(username);
+
+            if (optionalUser.isEmpty()) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            User user = optionalUser.get();
+
+            if (jwtService.isTokenValid(jwt, user)) {
+
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(
+                                user,
+                                null,
+                                Collections.emptyList()
+                        );
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+
+
+            filterChain.doFilter(request, response);
+
+        }
+
+    }
+
 }
